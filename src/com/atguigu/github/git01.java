1bd45eaa98e3bfa312cc851df99368dc26bf0d53
su <741929893@qq.com>
2015-09-08 19:02:11 +0800

    update git01_3

--- src/com/atguigu/github/git01.java
+++ src/com/atguigu/github/git01.java
@@ -9,5 +9,6 @@ public class git01 {
 		System.out.println("github to 0423  02");
 		System.out.println("github to 0423  03");
 		System.out.println("local to 0423 02");
+		System.out.println("local to 0423 03");
 	}
 }
