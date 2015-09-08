7546f8b57e185ca50bc73bbf7a32ff332b12ea0a
su <741929893@qq.com>
2015-09-08 18:34:11 +0800

    update git01_2

--- src/com/atguigu/github/git01.java
+++ src/com/atguigu/github/git01.java
@@ -5,5 +5,6 @@ public class git01 {
 	public static void main(String[] args) {
 		System.out.println("welcome to atguigu 0423");
 		System.out.println("github to 0423");
+		System.out.println("local to 0423");
 	}
 }
